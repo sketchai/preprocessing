6cd6bea9fe61aea3881c18faf6d7640c10af77ee
Julien Moreau <julien-j.moreau@edf.fr>
2022-02-18 10:50:02 +0100

    added better mock sequences to test

--- tests/unit/test_count.py
+++ tests/unit/test_count.py
@@ -13,10 +13,21 @@ logger = logging.getLogger()
 class TestFilterCount(unittest.TestCase):
 
     def test_counter(self):
-        # n < max nodes, should not send KO
-        n_nodes = 4
-        self.mock_sequence_1 = [NodeOp(label=0)]*n_nodes + [EdgeOp(label=0, references=None)]*2
-        conf_dict_1 = {'min' : None, 'max' : 5, 'type': 'node'}
+        # n <= max nodes, should not send KO
+        n_nodes = 5
+        self.mock_sequence_1 = [
+            NodeOp(label=0),
+            EdgeOp(label=1, references=None),
+            EdgeOp(label=3, references=None),
+            NodeOp(label=2),
+            NodeOp(label=3),
+            NodeOp(label=1),
+            EdgeOp(label=2, references=None),
+            EdgeOp(label=0, references=None),
+            NodeOp(label=1),
+            EdgeOp(label=2, references=None),
+        ]
+        conf_dict_1 = {'min': None, 'max': 5, 'type': 'node'}
         filter1 = FilterCount(conf_filter=conf_dict_1)
         for op in self.mock_sequence_1:
             message = {'op': op}
@@ -26,10 +37,14 @@ class TestFilterCount(unittest.TestCase):
 
         self.assertIsNone(answer.get(KO_FILTER_TAG))
 
-
         # n < min edges, should send KO
-        self.mock_sequence_2 = [NodeOp(label=0)]*5
-        conf_dict_2 = {'min' : 1, 'max' : None, 'type' : 'edge'}
+        self.mock_sequence_2 = [
+            NodeOp(label=0),
+            NodeOp(label=2),
+            NodeOp(label=3),
+            NodeOp(label=1),
+        ]
+        conf_dict_2 = {'min': 1, 'max': None, 'type': 'edge'}
         filter2 = FilterCount(conf_filter=conf_dict_2)
         for op in self.mock_sequence_2:
             message = {'op': op}
@@ -37,5 +52,5 @@ class TestFilterCount(unittest.TestCase):
         self.assertEqual(filter2.count, 0)
         answer = filter2.last_process(message)
         tag = answer.get(KO_FILTER_TAG)
-        
+
         self.assertEqual(tag, filter2.name)
