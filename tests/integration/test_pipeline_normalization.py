1aadd79f0434ebfaf13a09d4be0568f777dc21be
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-19 10:32:07 +0200

    modify normalization pipeline

--- tests/integration/test_pipeline_normalization.py
+++ tests/integration/test_pipeline_normalization.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.append('src/sketchgraphs/')
+sys.path.append('sketch_data/')
 sys.path.append('src/filtering-pipeline')
 
 import unittest
 import logging
 
 from experiments.experiment_normalization import ExperimentNormalization
-from tests import MOCK_NORMALIZATION_PATH, MOCK_COARSE_PATH
+from tests import MOCK_NORMALIZATION_PATH, MOCK_EXCHANGEFORMAT_PATH
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger()
@@ -15,7 +15,7 @@ class TestExperimentNormalization(unittest.TestCase):
 
     def test_run_pipeline(self):
         experiment = ExperimentNormalization()
-        experiment.d_conf['SourceFromFlatArray']['parms']['file_path'] = MOCK_COARSE_PATH
+        experiment.d_conf['SourceFromFlatArray']['parms']['file_path'] = MOCK_EXCHANGEFORMAT_PATH
         experiment.d_conf['SinkSlices']['parms']['output_path'] = MOCK_NORMALIZATION_PATH
 
         experiment.run_pipeline()
