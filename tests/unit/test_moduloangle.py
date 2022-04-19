b6296abd905465e51dd479c069eb9a5b29728692
Frederique ROBIN <frederique.robin@edf.fr>
2022-04-19 10:35:27 +0200

    some additionnal modifi

--- tests/unit/test_moduloangle.py
+++ tests/unit/test_moduloangle.py
@@ -1,16 +1,16 @@
 import sys
-sys.path.append('src/sketchgraphs/')
 sys.path.append('src/filtering-pipeline/')
+sys.path.append('sketch_data/')
 
 import unittest
-import logging
 import numpy as np
 
-from src.filters.filter_moduloangle import FilterModuloAngle
-from sketchgraphs.data.sequence import EdgeOp, NodeOp, EntityType, ConstraintType
+from src.filters.on_exchangeformat.filter_moduloangle import FilterModuloAngle
+from src.utils.logger import logger
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
+from sketch_data.catalog_primitive import Arc
+from sketch_data.catalog_constraint import Angle
+
 
 
 class TestFilterModuloAngle(unittest.TestCase):
@@ -19,28 +19,26 @@ class TestFilterModuloAngle(unittest.TestCase):
 
         conf_filter = {'request':
             {
-                ('node', EntityType.Arc): ["startParam","endParam"],
-                ('edge', ConstraintType.Angle): "angle"
+                Arc: ["angle_start","angle_end"],
+                Angle: "angle"
             }
         }
         filter1 = FilterModuloAngle(conf_filter)
 
 
         # testing arc node
-        arc = NodeOp(label=EntityType.Arc, parameters={
-            'startParam': 38.2*np.pi,
-            'endParam': 38.5*np.pi,
-        })
-
+        arc = Arc(status_construction=False, center=[0., 5.], radius=1, angles=[38.2*np.pi, 38.5*np.pi])
         message = filter1.process({'op': arc})
         
         op = message.get('op')
-        self.assertAlmostEqual(op.parameters.get('startParam'), 0.2*np.pi)
-        self.assertAlmostEqual(op.parameters.get('endParam'), 0.5*np.pi)
+        angle_start = op.__dict__.get('angle_start')
+        angle_end = op.__dict__.get('angle_end')
+        self.assertAlmostEqual(angle_start, 0.2*np.pi)
+        self.assertAlmostEqual(angle_end, 0.5*np.pi)
 
         # testing angle constraint
-        angle = EdgeOp(label=ConstraintType.Angle, references= None, parameters={'angle': 51*np.pi})
+        angle = Angle(references= [1,2], angle= 51*np.pi)
         message = filter1.process({'op': angle})
 
         op = message.get('op')
-        self.assertAlmostEqual(op.parameters.get('angle'), np.pi)
+        self.assertAlmostEqual(op.__dict__.get('angle'), np.pi)
