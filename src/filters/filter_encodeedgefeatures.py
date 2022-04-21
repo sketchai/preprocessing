df5da0369c0e6b7d9e9a41a2a128c728ce03434d
Julien Moreau <julien-j.moreau@edf.fr>
2022-04-21 11:18:54 +0200

    modify encodeedgefeatures

--- src/filters/filter_encodeedgefeatures.py
+++ src/filters/filter_encodeedgefeatures.py
@@ -1,14 +1,12 @@
 from typing import Dict
-import logging
 import torch
 
 from filtering_pipeline.filters.abstract_filter import AbstractFilter
-from sketchgraphs.data.sequence import EdgeOp
+from sketch_data.constraint import Constraint
 from src.utils.maps import construct_edge_map, construct_edge_map
 from src.utils import discretization
+from src.utils.logger import logger
 
-logging.basicConfig(level=logging.DEBUG)
-logger = logging.getLogger()
 
 class FilterEncodeEdgeFeatures(AbstractFilter):
     """
@@ -20,13 +18,13 @@ class FilterEncodeEdgeFeatures(AbstractFilter):
         self.name = 'FilterEncodeEdgeFeatures'
         n_bins = conf_filter.get('n_bins', 50)
         l_keep_edge = conf_filter['l_keep_edge']
-        self.edge_idx_map = construct_edge_map(l_keep_edge)
+        self.edge_idx_map = construct_edge_map(l_keep_edge, subnode=True)
         self.params_edge = discretization.create_params_edge(n_bins)
 
     def process(self, message: object) -> object:
         sequence = message.get('sequence')
-        edge_ops = [op for op in sequence if isinstance(op, EdgeOp)]
-        edge_features = torch.tensor([self.edge_idx_map[op.label.name] for op in edge_ops], dtype=torch.int64)
+        edge_ops = [op for op in sequence if isinstance(op, Constraint)]
+        edge_features = torch.tensor([self.edge_idx_map[op.type.name] for op in edge_ops], dtype=torch.int64)
         sparse_edge_features = discretization.discretization_edges(edge_ops, self.params_edge)
         message['edge_ops'] = edge_ops
         message['edge_features'] = edge_features
