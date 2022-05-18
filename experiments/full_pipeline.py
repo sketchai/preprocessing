cdc5364d1cbf215cd920e00d51ee729489cdf7b0
Julien Moreau <julien-j.moreau@edf.fr>
2022-05-18 15:36:48 +0200

    add merge and split

--- experiments/full_pipeline.py
+++ experiments/full_pipeline.py
@@ -21,6 +21,7 @@ from experiments.experiment_normalization import ExperimentNormalization
 from experiments.experiment_weight import ExperimentClusterOrder, ExperimentClusterParams
 from experiments.experiment_encoding import ExperimentEncoding
 from experiments.preprocessing_params import export_parameters
+from experiments.split_dataset import main as split_dataset
 
 logger = logging.getLogger()
 
@@ -34,6 +35,8 @@ def main():
     ExperimentClusterOrder(dataset=args.dataset).run_pipeline()
     ExperimentClusterParams(dataset=args.dataset).run_pipeline()
     ExperimentEncoding(dataset=args.dataset).run_pipeline()
+    if args.dataset=='merged':
+        split_dataset()
     export_parameters()
 
 if __name__ == '__main__':
